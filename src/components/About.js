7fa400317f43c45327be64459677878a4caf0f3a
Gad Dayan <70294896+gadou86@users.noreply.github.com>
2021-05-03 11:52:02 -0400

    added slug page for post

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import sanityClient from '../client.js';
+
 
 function About() {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    sanityClient.fetch(`*[_type == "author"]{
+      name,
+      bio,
+      "authoImage": image.asset->url
+    }`).then((data) => setAuthor(data[0]))
+    .catch(console.error)
+  }, []);
+
+  if(!author) return <div>Loading...</div>
+
   return (
     <div>
       <h1>IM ABOUT</h1>
